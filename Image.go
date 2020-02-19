56eb4a90376c3b5470c1de57ec43311538050653
onokatio <onokatio@gmail.com>
2020-02-19 20:22:55 +0900

    impl image

--- Image.go
+++ Image.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"context"
-	"google.golang.org/grpc"
+	"fmt"
+	"os"
         "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	pdeployment "github.com/n0stack/n0stack/n0proto.go/deployment/v0"
+	"gopkg.in/yaml.v3"
 )
 
 func resource_n0stack_image() *schema.Resource {
@@ -34,37 +34,85 @@ func resource_n0stack_image() *schema.Resource {
 	}
 }
 
+type ImageTask struct {
+	Type        string      `yaml:"type"`
+	Action      string      `yaml:"action"`
+	Args        struct {
+		Name           string             `yaml:"name"`
+	}
+	DependsOn   []string    `yaml:"depends_on"`
+	IgnoreError bool        `yaml:"ignore_error"`
+	// Rollback []*Task `yaml:"rollback"`
+
+	child   []string
+	depends int
+}
+
+type ImageRegistryTask struct {
+	Type        string      `yaml:"type"`
+	Action      string      `yaml:"action"`
+	Args        struct {
+		ImageName           string             `yaml:"image_name"`
+		Tags                 []string           `yaml:"tags"`
+		BlockStorageName    string             `yaml:"blockstorage_name"`
+	}
+	DependsOn   []string    `yaml:"depends_on"`
+	IgnoreError bool        `yaml:"ignore_error"`
+	// Rollback []*Task `yaml:"rollback"`
+
+	child   []string
+	depends int
+}
+
 func resource_n0stack_image_create(d *schema.ResourceData, meta interface{}) error {
-	config := meta.(Config)
-	conn, err := grpc.Dial(config.endpoint, grpc.WithInsecure())
+	task := ImageTask{}
+	task.Type = "Image"
+	task.Action = "GenerateBlockStorage"
+	task.Args.Name = d.Get("image_name").(string)
+
+	taskList := make(map[string]ImageTask)
+	taskList["ApplyImage-" + d.Get("image_name").(string)] = task
+
+	yamlString, err := yaml.Marshal(&taskList)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+	}
+
+	task_registry := ImageRegistryTask{}
+	task_registry.Type = "Image"
+	task_registry.Action = "RegisterBlockStorage"
+	task_registry.Args.ImageName = d.Get("image_name").(string)
+	task_registry.Args.Tags = interfaceList2stringList(d.Get("tags").([]interface{}))
+	task_registry.Args.BlockStorageName = d.Get("blockstorage_name").(string)
+
+	taskList_registry := make(map[string]ImageRegistryTask)
+	taskList_registry["ApplyImage-" + d.Get("image_name").(string)] = task_registry
+
+	yamlString_registry, err := yaml.Marshal(&taskList_registry)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+	}
+
+	err = os.MkdirAll("n0cli-yaml/Generate", 0755)
 	if err != nil {
 		return err
 	}
-	defer conn.Close()
-
-	client := pdeployment.NewImageServiceClient(conn)
-
-	{
-		request := pdeployment.ApplyImageRequest{
-			Name: d.Get("image_name").(string) ,
-		}
-		_, err = client.ApplyImage(context.Background(), &request)
-		if err != nil {
-			return err
-		}
+	err = os.MkdirAll("n0cli-yaml/Delete", 0755)
+	if err != nil {
+		return err
 	}
 
-	{
-		request := pdeployment.RegisterBlockStorageRequest{
-			ImageName: d.Get("image_name").(string) ,
-			BlockStorageName: d.Get("blockstorage_name").(string),
-			Tags: interfaceList2stringList(d.Get("tags").([]interface{})),
-		}
-		_, err = client.RegisterBlockStorage(context.Background(), &request)
-		if err != nil {
-			return err
-		}
+	file, err := os.Create("n0cli-yaml/Generate/ApplyImage-" + d.Get("image_name").(string) + ".yaml")
+	if err != nil {
+		return err;
 	}
+	file_registry, err := os.Create("n0cli-yaml/Generate/RegisterBlockStorage-" + d.Get("image_name").(string) + "to" + d.Get("blockstorage_name").(string) + ".yaml")
+	if err != nil {
+		return err;
+	}
+
+	fmt.Fprint(file, string(yamlString))
+	fmt.Fprint(file_registry, string(yamlString_registry))
 
 	d.SetId(d.Get("image_name").(string))
 
@@ -72,26 +120,6 @@ func resource_n0stack_image_create(d *schema.ResourceData, meta interface{}) err
 }
 
 func resource_n0stack_image_read(d *schema.ResourceData, meta interface{}) error {
-	config := meta.(Config)
-	conn, err := grpc.Dial(config.endpoint, grpc.WithInsecure())
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-
-	client := pdeployment.NewImageServiceClient(conn)
-
-	request := pdeployment.GetImageRequest{
-		Name: d.Get("image_name").(string) ,
-	}
-	res, err := client.GetImage(context.Background(), &request)
-	if err != nil {
-		return err
-	}
-
-	d.Set("image_name",res.Name)
-	d.Set("tags",res.Tags)
-	//d.Set("blockstorage_name",res.RegisteredBlockStorages)
 	return nil
 }
 
@@ -100,22 +128,5 @@ func resource_n0stack_image_update(d *schema.ResourceData, meta interface{}) err
 }
 
 func resource_n0stack_image_delete(d *schema.ResourceData, meta interface{}) error {
-	config := meta.(Config)
-	conn, err := grpc.Dial(config.endpoint, grpc.WithInsecure())
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-
-	client := pdeployment.NewImageServiceClient(conn)
-
-	request := pdeployment.DeleteImageRequest{
-		Name: d.Get("image_name").(string) ,
-	}
-	_, err = client.DeleteImage(context.Background(), &request)
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
