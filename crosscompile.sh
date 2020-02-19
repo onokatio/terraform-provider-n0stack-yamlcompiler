33ca2bb25a7e38bbc461f1d01a703b253ace85a4
onokatio <onokatio@gmail.com>
2020-02-20 00:24:34 +0900

    change binary name

--- crosscompile.sh
+++ crosscompile.sh
@@ -3,10 +3,10 @@
 GOARCH=amd64
 
 GOOS=darwin
-go build -o terraform-provider-n0stack-$GOOS-$GOARCH
+go build -o terraform-provider-n0stack-yamlcompiler-$GOOS-$GOARCH
 
 GOOS=linux
-go build -o terraform-provider-n0stack-$GOOS-$GOARCH
+go build -o terraform-provider-n0stack-yamlcompiler-$GOOS-$GOARCH
 
 GOOS=windows
-go build -o terraform-provider-n0stack-$GOOS-$GOARCH
+go build -o terraform-provider-n0stack-yamlcompiler-$GOOS-$GOARCH
