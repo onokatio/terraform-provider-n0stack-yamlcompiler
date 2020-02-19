8460f42d38e2c8ce1e51ed24e3f0633b8debe5b3
onokatio <onokatio@gmail.com>
2020-02-19 19:26:49 +0900

    impl Network

--- Network.go
+++ Network.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"context"
-	"google.golang.org/grpc"
+	"fmt"
+	"os"
+	//"google.golang.org/grpc"
         "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	ppool "github.com/n0stack/n0stack/n0proto.go/pool/v0"
+	//ppool "github.com/n0stack/n0stack/n0proto.go/pool/v0"
+	"gopkg.in/yaml.v3"
 )
 
 func resource_n0stack_network() *schema.Resource {
@@ -41,56 +43,63 @@ func resource_n0stack_network() *schema.Resource {
 	}
 }
 
+type NetworkTask struct {
+	Type        string      `yaml:"type"`
+	Action      string      `yaml:"action"`
+	Args        struct {
+		Name           string             `yaml:"name"`
+		Annotations         map[string]string  `yaml:"annotations"`
+		Labels              map[string]string  `yaml:"labels"`
+		Ipv4Cidr           string             `yaml:"ipv4_cidr"`
+		Ipv6Cidr           string             `yaml:"ipv6_cidr"`
+	}
+	DependsOn   []string    `yaml:"depends_on"`
+	IgnoreError bool        `yaml:"ignore_error"`
+	// Rollback []*Task `yaml:"rollback"`
+
+	child   []string
+	depends int
+}
+
 func resource_n0stack_network_create(d *schema.ResourceData, meta interface{}) error {
-	config := meta.(Config)
-	conn, err := grpc.Dial(config.endpoint, grpc.WithInsecure())
+	task := NetworkTask{}
+	task.Type = "Network"
+	task.Action = "ApplyNetwork"
+	task.Args.Name = d.Get("name").(string)
+	task.Args.Annotations = interfaceMap2stringMap(d.Get("annotations").(map[string]interface{}))
+	task.Args.Labels = interfaceMap2stringMap(d.Get("labels").(map[string]interface{}))
+	task.Args.Ipv4Cidr = d.Get("ipv4_cidr").(string)
+	task.Args.Ipv6Cidr = d.Get("ipv6_cidr").(string)
+
+	taskList := make(map[string]NetworkTask)
+	taskList["ApplyNetwork-" + d.Get("name").(string)] = task
+
+	yamlString, err := yaml.Marshal(&taskList)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+	}
+
+	err = os.MkdirAll("n0cli-yaml/Generate", 0755)
 	if err != nil {
 		return err
 	}
-	defer conn.Close()
-
-	client := ppool.NewNetworkServiceClient(conn)
-
-	request := ppool.ApplyNetworkRequest{
-		Name: d.Get("name").(string),
-		Ipv4Cidr: d.Get("ipv4_cidr").(string),
-		Ipv6Cidr: d.Get("ipv6_cidr").(string),
-		Annotations: interfaceMap2stringMap(d.Get("annotations").(map[string]interface{})),
-		Labels: interfaceMap2stringMap(d.Get("labels").(map[string]interface{})),
-	}
-	_, err = client.ApplyNetwork(context.Background(), &request)
+	err = os.MkdirAll("n0cli-yaml/Delete", 0755)
 	if err != nil {
 		return err
 	}
 
+	file, err := os.Create("n0cli-yaml/Generate/Network-" + d.Get("name").(string) + ".yaml")
+	if err != nil {
+		return err;
+	}
+
+	fmt.Fprint(file, string(yamlString))
 	d.SetId(d.Get("name").(string))
 
 	return resource_n0stack_network_read(d, meta)
 }
 
 func resource_n0stack_network_read(d *schema.ResourceData, meta interface{}) error {
-	config := meta.(Config)
-	conn, err := grpc.Dial(config.endpoint, grpc.WithInsecure())
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-
-	client := ppool.NewNetworkServiceClient(conn)
-
-	request := ppool.GetNetworkRequest{
-		Name: d.Get("name").(string) ,
-	}
-	res, err := client.GetNetwork(context.Background(), &request)
-	if err != nil {
-		return err
-	}
-
-	d.Set("name",res.Name)
-	d.Set("ipv4_cidr",res.Ipv4Cidr)
-	d.Set("ipv6_cidr",res.Ipv6Cidr)
-	d.Set("annotations", res.Annotations)
-	d.Set("labels", res.Labels)
 	return nil
 }
 
@@ -99,22 +108,5 @@ func resource_n0stack_network_update(d *schema.ResourceData, meta interface{}) e
 }
 
 func resource_n0stack_network_delete(d *schema.ResourceData, meta interface{}) error {
-	config := meta.(Config)
-	conn, err := grpc.Dial(config.endpoint, grpc.WithInsecure())
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-
-	client := ppool.NewNetworkServiceClient(conn)
-
-	request := ppool.DeleteNetworkRequest{
-		Name: d.Get("name").(string) ,
-	}
-	_, err = client.DeleteNetwork(context.Background(), &request)
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
